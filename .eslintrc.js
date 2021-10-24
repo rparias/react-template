373168792979b15af65f40d9a39172b49790c85a
Ronald Arias <ariasron@hotmail.com>
2021-10-24 13:46:31 -0500

    Installs eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'standard',
+    'react-app',
+    'react-app/jest',
+    'plugin:react/recommended',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'testing-library', 'jest-dom'],
+  rules: {
+    semi: ['error', 'never'],
+    'space-before-function-paren': ['error', 'never'],
+  },
+}
